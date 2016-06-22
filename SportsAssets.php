403b2025fed09e5fdc6752dc6869e44e0a5afcc2
Qiang Li <joneleesz@hotmail.com>
2016-06-22 17:45:35 +0100

    add a new assets file.

---
+++ SportsAssets.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: qiang
+ * Date: 5/23/16
+ * Time: 3:40 PM
+ */
+
+namespace frontend\assets;
+use yii\web\AssetBundle;
+
+
+class SportsAssets extends AssetBundle
+{
+    //public $basePath = '@webroot';
+    public $sourcePath = '@web/assets';
+
+    public $css = [
+        'css/main.css',
+        'css/bootstrap.min.css',
+        'css/bootstrap-theme.css',
+        'css/font-awesome.min.css',
+    ];
+
+    public $js = [
+        'js/headroom.min.js',
+        'js/jQuery.headroom.min.js',
+        'js/respond.min.js',
+        'js/template.js',
+    ];
+    public $depends = [
+    ];
+
+}
