013c7472c00eebfec7ffabcad8875a1ac9ae8cfc
Qiang Li <joneleesz@hotmail.com>
2016-06-23 11:30:36 +0100

    add error page...

---
+++ views/default/error.php
@@ -0,0 +1,13 @@
+<?php
+//$this->controller->layout = 'single';
+?>
+
+<div style="margin:100px 30px">
+    <h3>
+        Er, something is wrong!
+    </h3>
+
+    <h4>
+        This does not exists, or something is wrong, please go to <a href="<?=Yii::$app->homeUrl?>">Home </a> page or contact the owner of this website.
+    </h4>
+</div>
